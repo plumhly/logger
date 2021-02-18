12162d1d81906cd0163b11d5f7e70844e355e2ab
bo.li <bo.li@100class.com>
2021-02-18 15:20:42 +0800

    fix: color adapter

--- lib/src/ansi_color.dart
+++ lib/src/ansi_color.dart
@@ -1,44 +1,55 @@
-/// This class handles colorizing of terminal output.
 class AnsiColor {
   /// ANSI Control Sequence Introducer, signals the terminal for new settings.
   static const ansiEsc = '\x1B[';
 
   /// Reset all colors and options for current SGRs to terminal defaults.
-  static const ansiDefault = '${ansiEsc}0m';
+  static const ansiDefault = "${ansiEsc}0m";
 
-  final int? fg;
-  final int? bg;
+  final int fg;
+  final int bg;
   final bool color;
+  final String prefix;
 
   AnsiColor.none()
       : fg = null,
         bg = null,
-        color = false;
+        color = false,
+        prefix = "";
 
   AnsiColor.fg(this.fg)
       : bg = null,
-        color = true;
+        color = true,
+        prefix = "";
 
   AnsiColor.bg(this.bg)
       : fg = null,
-        color = true;
+        color = true,
+        prefix = "";
 
-  @override
+  AnsiColor.txt(this.prefix)
+      : fg = null,
+        bg = null,
+        color = false;
+
   String toString() {
-    if (fg != null) {
-      return '${ansiEsc}38;5;${fg}m';
-    } else if (bg != null) {
-      return '${ansiEsc}48;5;${bg}m';
+    if (color) {
+      if (fg != null) {
+        return "${ansiEsc}38;5;${fg}m";
+      } else if (bg != null) {
+        return "${ansiEsc}48;5;${bg}m";
+      } else {
+        return "";
+      }
     } else {
-      return '';
+      return prefix;
     }
   }
 
   String call(String msg) {
     if (color) {
-      return '${this}$msg$ansiDefault';
+      return "${this}$msg$ansiDefault";
     } else {
-      return msg;
+      return "${this}$msg";
     }
   }
 
@@ -47,10 +58,10 @@ class AnsiColor {
   AnsiColor toBg() => AnsiColor.bg(fg);
 
   /// Defaults the terminal's foreground color without altering the background.
-  String get resetForeground => color ? '${ansiEsc}39m' : '';
+  String get resetForeground => color ? "${ansiEsc}39m" : "";
 
   /// Defaults the terminal's background color without altering the foreground.
-  String get resetBackground => color ? '${ansiEsc}49m' : '';
+  String get resetBackground => color ? "${ansiEsc}49m" : "";
 
   static int grey(double level) => 232 + (level.clamp(0.0, 1.0) * 23).round();
 }
