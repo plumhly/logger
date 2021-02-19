eb6651b18f389d9b86b1ca409761a35b86e5f34e
bo.li <bo.li@100class.com>
2021-02-19 11:34:34 +0800

    fix: 修改info颜色值

--- lib/src/printers/pretty_printer.dart
+++ lib/src/printers/pretty_printer.dart
@@ -27,7 +27,7 @@ class PrettyPrinter extends LogPrinter {
   static final levelColors = {
     Level.verbose: AnsiColor.fg(AnsiColor.grey(0.5)),
     Level.debug: AnsiColor.none(),
-    Level.info: AnsiColor.fg(12),
+    Level.info: AnsiColor.fg(2),
     Level.warning: AnsiColor.fg(208),
     Level.error: AnsiColor.fg(196),
     Level.wtf: AnsiColor.fg(199),
